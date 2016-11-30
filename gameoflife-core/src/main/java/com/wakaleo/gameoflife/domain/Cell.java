85fe0f779cc65f1ba799a570121f8c01b63811b2
Manu García <manuelbadman@gmail.com>
2016-11-30 19:29:54 +0100

    Cambiados de nuevo

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -1,8 +1,8 @@
 package com.wakaleo.gameoflife.domain;
 
 public enum Cell {
-	//Se ha cambiado
-    LIVE_CELL("+"), DEAD_CELL(".");
+	
+    LIVE_CELL("*"), DEAD_CELL(".");
 
     private String symbol;
 
